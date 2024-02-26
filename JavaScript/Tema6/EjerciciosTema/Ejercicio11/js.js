908ed762f641142da1a6bba030b1a2d023b85f14
sergiocuacor <2sergiocuadrado2000@gmail.com>
2024-02-26 12:42:15 +0100

    kdfshk

--- JavaScript/Tema6/EjerciciosTema/Ejercicio11/js.js
+++ JavaScript/Tema6/EjerciciosTema/Ejercicio11/js.js
@@ -6,3 +6,26 @@ un elemento que muestre el valor Cuatro.
  Dentro del elemento Cuatro, añadirle un hijo de tipo párrafo con el texto “Soy hijo
 de Cuatro”.
  Cambiar el contenido del título por “He creado una lista”*/
+
+var lista = document.createElement("ol");
+
+document.body.appendChild(lista);
+
+var elemento1 = document.createElement("li");
+
+elemento2.innerHTML = "Uno";
+lista.appendChild(elemento1);
+
+var elemento2 = document.createElement("li");
+
+elemento2.innerHTML = "Dos";
+
+lista.appendChild(elemento2);
+
+elemento3.innerHTML = "Tres";
+
+var elemento3 = document.createElement("li");
+
+lista.appendChild(elemento3);
+
+
