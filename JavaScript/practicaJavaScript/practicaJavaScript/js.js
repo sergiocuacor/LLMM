d43313f519b23986e4b7603fb2108db18af1baa7
sergiocuacor <2sergiocuadrado2000@gmail.com>
2024-04-03 13:06:37 +0200

    ejercicioCorona PT3

---
+++ JavaScript/practicaJavaScript/practicaJavaScript/js.js
@@ -0,0 +1,54 @@
+function actualizarTextoComoActuar() {
+    let divComoActuar = document.getElementById("divComoActuar");
+    let checkFiebre = document.getElementById("fiebre").checked; // El .checked es un booleano
+    let checkTos = document.getElementById("tos").checked;
+    let checkRespirar = document.getElementById("respirar").checked;
+    let checkGrave = document.getElementById("grave").checked;
+
+    divComoActuar.classList.add("estiloAzulNegrita");
+
+    if ((checkFiebre || checkTos) && (checkRespirar || checkGrave)) {
+
+        divComoActuar.innerText = " Llama al tlf 112..";
+
+    } else if (checkFiebre || checkTos) {
+
+        divComoActuar.innerText = "Quédate en casa..";
+
+    } else {
+        divComoActuar.innerText = "";
+    }
+
+
+
+}
+
+function incluirTituloEnDiv() {
+    let divParaTodos = document.getElementById("paraTodos");
+    let titulo = document.createElement("h2");
+
+    titulo.innerText = "a. ¿Cómo puedo protegerme?";
+    divParaTodos.appendChild(titulo);
+
+    titulo.onmouseover = añadirAlH2;
+}
+
+function añadirAlH2() {
+
+    let titulo = document.getElementsByTagName("h2")[0];
+
+    let parrafo1 = document.createElement("p");
+    parrafo1.innerText = "i. Lávate las manos frecuentemente con agua y jabón.";
+
+    let parrafo2 = document.createElement("p");
+    parrafo2.innerText = "ii. Al toser o estornudar, cúbrete la boca y la nariz con el codo flexionado o con un pañuelo desechable y tíralo a la basura.";
+
+    let parrafo3 = document.createElement("p");
+    parrafo3.innerText = "iii. Evita tocarte los ojos, la nariz y la boca, y que las manos facilitan la transmisión.";
+
+    titulo.appendChild(parrafo1);
+    titulo.appendChild(parrafo2);
+    titulo.appendChild(parrafo3);
+
+
+}
